ed16adf5fd4081980f800268976eac6d4e09d1f9
william gabriel Pereira <will.gabriel.pereira@gmail.com>
2026-04-16 23:20:52 -0300

    Criado .env

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,7 +3,7 @@ import { useAuthStore } from '../stores/auth';
 import router from '../router';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080',
   headers: {
     'Content-Type': 'application/json'
   }
@@ -55,7 +55,7 @@ api.interceptors.response.use(response => {
 
     const authStore = useAuthStore();
     try {
-      const response = await axios.post('http://localhost:8080/auth/refresh', {
+      const response = await axios.post(`${import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080'}/auth/refresh`, {
         refresh_token: authStore.refreshToken
       });
       
